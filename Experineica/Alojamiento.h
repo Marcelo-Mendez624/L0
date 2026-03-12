d7bbb30e399f385188d60d56efc46af18f6bc7b2
Marcelo Mendez <83145704+Marcelo-Mendez624@users.noreply.github.com>
2026-03-12 11:48:58 -0300

    Cambios json

--- Experineica/Alojamiento.h
+++ Experineica/Alojamiento.h
@@ -1,14 +1,21 @@
+#include <string>
 #include "Experiencia.h"
 #include "Data/Regimen.h"
 
-class Alojamiento : Experiencia
+class Alojamiento : public Experiencia 
 {
+private:
     std::string hotel;
     TipoRegimen regimen;
     int cantNoches;
 
 public:
-    std::string getHotel() { return hotel; }
+    Alojamiento(std::string codigo, std::string desc, int precioBase, DTFecha fecha,
+                std::string hotel, TipoRegimen regimen, int cantNoches); 
+
+    virtual ~Alojamiento();
+
+    std::string getHotel() const { return hotel; }
 
     virtual float calcularCosto();
 };
--- main.cpp
+++ main.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
 #include <list>
 #include <map>
-#include "Experiencia.h"
-#include "Turista.h"
-#include "Data/Regimen.h"
+#include <string>
+#include <set>
 
+#include "Turista.h"
+
+// EXPERIENCIAS
+#include "Experiencia.h"
+#include "Alojamiento.h"
+
+//  DATA TYPES
+#include "DTFecha.h"
+#include "Data/Regimen.h"
 std::list<Experiencia*> experiencias;
 std::map<std::string, Experiencia*> map_experiencias;
 
@@ -37,8 +45,15 @@ Experiencia* coleccion_getExperiencia(std::string codigoReserva){
 
 void parte_a()
 {
-	class Alojamiento alojamiento1("ALX5489", "Hotel moderno", 30, "18/05/2020", "Hotel Lindorf", 
-		AllInclusive, 5);
+	Alojamiento* alojamiento1 = new Alojamiento(
+    "ALX5489", 
+    "Hotel moderno", 
+    30, 
+    DTFecha(18, 5, 2020), 
+    "Hotel Lindorf", 
+    AllInclusive, 
+    5
+);
 }
 
 void parte_b(){
